d7b51b15584d94bc8cf2efc9b4b11209883527a3
LDudanova <l.dudanova92@gmail.com>
2025-09-18 18:58:23 +0300

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom"
 import {useTranslation} from "react-i18next";
 import "./i18n";
 import "./App.css"
@@ -12,15 +13,20 @@ function App() {
 
     return (
         <>
-            <HomePage/>
-            <div>
-                <button onClick={() => switchLanguage("en")}>EN</button>
-                <button onClick={() => switchLanguage("ru")}>RU</button>
+            <BrowserRouter>
+                <Routes>
+                    <Route path={"/"} element={<HomePage/>}/>
+                </Routes>
 
-                <p>{t("spent_on", { amount: 500, category: t("expense") })}</p>
+                <div>
+                    <button onClick={() => switchLanguage("en")}>EN</button>
+                    <button onClick={() => switchLanguage("ru")}>RU</button>
 
-                <button>{t("add_transaction")}</button>
-            </div>
+                    <p>{t("spent_on", {amount: 500, category: t("expense")})}</p>
+
+                    <button>{t("add_transaction")}</button>
+                </div>
+            </BrowserRouter>
         </>
     )
 }
